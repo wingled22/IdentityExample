1671f68c34bf3fe6ee82206fda8dcd8bb08e2bd2
wingled22 <pelayowindel@gmail.com>
2023-09-10 11:20:42 +0800

    added boilerplate codes

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace IdentitySample.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
